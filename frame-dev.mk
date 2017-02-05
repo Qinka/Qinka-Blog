7d9ad5f5dee4ccdd7bbd37597267378496676da8
Qinka <qinka@live.com>
2017-02-05 22:19:20 +0800

    update

--- frame-dev.mk
+++ frame-dev.mk
@@ -6,12 +6,14 @@ blog.post: blog.html
 		' -F "title=Blog" ' \
 		' -F "html=@blog.html" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
-		 $(SITE_URL)/blog  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+		' -F "tag=page" ' \
+		 $(SITE_URL)/blog  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 blog.post.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=post" ' \
-		 $(SITE_URL)/blog  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/blog  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 blog.script: script/blog.js 
@@ -23,12 +25,13 @@ blog.script: script/blog.js
 		' -F "text=@script/blog.js" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
 		' -F "tag=script" ' \
-		 $(SITE_URL)/script/blog  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+		 $(SITE_URL)/script/blog  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 blog.script.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=text" ' \
-		 $(SITE_URL)/script/blog  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/script/blog  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 bottom.frame: frame-bottom.html 
@@ -40,12 +43,13 @@ bottom.frame: frame-bottom.html
 		' -F "html=@frame-bottom.html" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
 		' -F "tag=frame" ' \
-		 $(SITE_URL)/~@123bottom  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+		 $(SITE_URL)/~@123bottom  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 bottom.frame.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=frame" ' \
-		 $(SITE_URL)/~@123bottom  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/~@123bottom  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 code.style.sheet: style-sheet/code.glob.css 
@@ -57,12 +61,13 @@ code.style.sheet: style-sheet/code.glob.css
 		' -F "text=@style-sheet/code.glob.css" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
 		' -F "tag=style-sheet" ' \
-		 $(SITE_URL)/highlight/default  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+		 $(SITE_URL)/highlight/default  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 code.style.sheet.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=text" ' \
-		 $(SITE_URL)/highlight/default  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/highlight/default  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 fork.me.img: fork.me.png 
@@ -74,12 +79,13 @@ fork.me.img: fork.me.png
 		' -F "binary=@fork.me.png" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
 		' -F "tag=frame" ' \
-		 $(SITE_URL)/img/fork.me.png  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+		 $(SITE_URL)/img/fork.me.png  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 fork.me.img.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=binary" ' \
-		 $(SITE_URL)/img/fork.me.png  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/img/fork.me.png  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 frame.js.text: script/frame.js 
@@ -91,12 +97,13 @@ frame.js.text: script/frame.js
 		' -F "text=@script/frame.js" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
 		' -F "tag=script" ' \
-		 $(SITE_URL)/script/frame.js  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+		 $(SITE_URL)/script/frame.js  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 frame.js.text.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=text" ' \
-		 $(SITE_URL)/script/frame.js  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/script/frame.js  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 glob.icon.binary: glob.ico 
@@ -108,12 +115,13 @@ glob.icon.binary: glob.ico
 		' -F "binary=@glob.ico" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
 		' -F "tag=icon" ' \
-		 $(SITE_URL)/favicon.ico  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+		 $(SITE_URL)/favicon.ico  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 glob.icon.binary.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=binary" ' \
-		 $(SITE_URL)/favicon.ico  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/favicon.ico  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 hack.theme: style-sheet/hack.css 
@@ -125,12 +133,13 @@ hack.theme: style-sheet/hack.css
 		' -F "text=@style-sheet/hack.css" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
 		' -F "tag=theme" ' \
-		 $(SITE_URL)/theme/hack  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+		 $(SITE_URL)/theme/hack  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 hack.theme.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=text" ' \
-		 $(SITE_URL)/theme/hack  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/theme/hack  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 index.post: index.md 
@@ -142,64 +151,13 @@ index.post: index.md
 		' -F "title=Home" ' \
 		' -F "html=@.ignore/index.html" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
-		 $(SITE_URL)/  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+		 $(SITE_URL)/  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 index.post.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=post" ' \
-		 $(SITE_URL)/  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
-
-
-nav.frame: frame-nav.html 
-	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X PUT  \
-		' -F "type=frame" ' \
-		' -F "create-time=2017-02-02 08:41:39.658135 UTC" ' \
-		' -F "update-time=$(IH_NOW)" ' \
-		' -F "title=nav.frame" ' \
-		' -F "html=@frame-nav.html" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
-		' -F "tag=frame" ' \
-		 $(SITE_URL)/~@123nav  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
-
-nav.frame.del:
-	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
-		' -F "type=frame" ' \
-		 $(SITE_URL)/~@123nav  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
-
-
-top.frame: frame-top.html 
-	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X PUT  \
-		' -F "type=frame" ' \
-		' -F "create-time=2017-02-02 08:31:04.495661 UTC" ' \
-		' -F "update-time=$(IH_NOW)" ' \
-		' -F "title=top.frame" ' \
-		' -F "html=@frame-top.html" ' \
-		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
-		' -F "tag=frame" ' \
-		 $(SITE_URL)/~@123top  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
-
-top.frame.del:
-	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
-		' -F "type=frame" ' \
-		 $(SITE_URL)/~@123top  |  $(IH_PATH) -m  -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
-
-
-qrcode.js: script/qrcode.min.js 
-	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X PUT  \
-		' -F "type=text" ' \
-		' -F "create-time=2017-02-04 01:02:19.788994 UTC" ' \
-		' -F "update-time=$(IH_NOW)" ' \
-		' -F "title=QRCode in js" ' \
-		' -F "text=@script/qrcode.min.js" ' \
-		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
-		' -F "whose=script/qrcode.min.js" ' \
-		' -F "tag=script" ' \
-		 $(SITE_URL)/script/qrcode.js  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
-
-qrcode.js.del:
-	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
-		' -F "type=text" ' \
-		 $(SITE_URL)/script/qrcode.js  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		 $(SITE_URL)/  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 license.post: LICENSE.md 
@@ -218,7 +176,8 @@ license.post: LICENSE.md
 license.post.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=post" ' \
-		 $(SITE_URL)/license  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/license  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
 menu.post: Menu.md 
@@ -237,6 +196,62 @@ menu.post: Menu.md
 menu.post.del:
 	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
 		' -F "type=post" ' \
-		 $(SITE_URL)/menu  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DETAL)' -v | $(SHELL)
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/menu  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+
+
+nav.frame: frame-nav.html 
+	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X PUT  \
+		' -F "type=frame" ' \
+		' -F "create-time=2017-02-02 08:41:39.658135 UTC" ' \
+		' -F "update-time=$(IH_NOW)" ' \
+		' -F "title=nav.frame" ' \
+		' -F "html=@frame-nav.html" ' \
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		' -F "tag=frame" ' \
+		 $(SITE_URL)/~@123nav  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+
+nav.frame.del:
+	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
+		' -F "type=frame" ' \
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/~@123nav  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+
+
+qrcode.js: script/qrcode.min.js 
+	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X PUT  \
+		' -F "type=text" ' \
+		' -F "create-time=2017-02-04 01:02:19.788994 UTC" ' \
+		' -F "update-time=$(IH_NOW)" ' \
+		' -F "title=QRCode in js" ' \
+		' -F "text=@script/qrcode.min.js" ' \
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		' -F "whose=script/qrcode.min.js" ' \
+		' -F "tag=script" ' \
+		 $(SITE_URL)/script/qrcode.js  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+
+qrcode.js.del:
+	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
+		' -F "type=text" ' \
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/script/qrcode.js  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+
+
+top.frame: frame-top.html 
+	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X PUT  \
+		' -F "type=frame" ' \
+		' -F "create-time=2017-02-02 08:31:04.495661 UTC" ' \
+		' -F "update-time=$(IH_NOW)" ' \
+		' -F "title=top.frame" ' \
+		' -F "html=@frame-top.html" ' \
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		' -F "tag=frame" ' \
+		 $(SITE_URL)/~@123top  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
+
+top.frame.del:
+	@$(ECHO) $(CURL_PATH) $(CURL_DETAIL)  -X DELETE  \
+		' -F "type=frame" ' \
+		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
+		 $(SITE_URL)/~@123top  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 
