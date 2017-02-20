9842b1d66792e2560ccd2b675576d8091e1ed352
Qinka <qinka@live.com>
2017-02-20 22:23:54 +0800

    update;change get start 1\'s title

--- post-ddopsnd.mk
+++ post-ddopsnd.mk
@@ -5,13 +5,14 @@ getstart.1.ddopsnd.post: post/ddopsnd/get-start.1.md  post/ddopsnd/get-start.1.s
 		' -F "type=post" ' \
 		' -F "create-time=2017-02-18 12:50:04.974444 UTC" ' \
 		' -F "update-time=$(IH_NOW)" ' \
-		' -F "title=Get Start 1" ' \
+		' -F "title=Ddopsnd: Get Start 1" ' \
 		' -F "html=@.ignore/get-start.1.html" ' \
 		' -F "sha-file-name=/`$(MD5) $(PRIVATE_KEY).pub`" ' \
 		' -F "summary=@.ignore/get-start.1.sum.html" ' \
 		' -F "whose=Qinka" ' \
 		' -F "tag=blog" ' \
 		' -F "tag=code" ' \
+		' -F "tag=highlight" ' \
 		 $(SITE_URL)/b/ddopsnd/getstart/1  |  $(IH_PATH) -m  -f '$(IH_DELAY)' -p '$(PRIVATE_KEY)' -d '$(SITE_DELTA)' -v | $(SHELL)
 
 getstart.1.ddopsnd.post.del:
